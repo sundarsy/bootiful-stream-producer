b11227d0acdd35f048d6360abd163a85b9b2af45
zeagord <sundarsy@gmail.com>
2018-06-18 15:04:10 +0700

    Switches to Springboot2

--- src/main/java/com/github/sundarsy/producer/StreamProducerApplication.java
+++ src/main/java/com/github/sundarsy/producer/StreamProducerApplication.java
@@ -3,7 +3,8 @@ package com.github.sundarsy.producer;
 import java.time.Instant;
 import java.time.LocalDate;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -18,7 +19,7 @@ import org.springframework.messaging.support.GenericMessage;
 @EnableBinding(Source.class)
 public class StreamProducerApplication {
 
-	protected Logger logger = Logger.getLogger(StreamProducerApplication.class.getName());
+	protected Logger logger = LoggerFactory.getLogger(StreamProducerApplication.class.getName());
 
 	private int index = 0;
 
@@ -27,7 +28,7 @@ public class StreamProducerApplication {
 	}
 	
 	@Bean	
-	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))
+	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "1000", maxMessagesPerPoll = "1"))
 	public MessageSource<String> orderSource() {
 		return () -> {
 			String str = "My Message" + Instant.now();
